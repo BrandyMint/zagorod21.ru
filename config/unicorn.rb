ef9d2075981e2b051134a50e86f11da696375ccb
Alexey Illarionov <littlesavage@rambler.ru>
2013-11-15 19:36:34 +0400

    delete incremental killoff

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -35,20 +35,6 @@ end
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
-
-  # Incremental kill-off
-  old_pid = "#{server.config[:pid]}.oldbin"
-  if old_pid != server.pid
-    begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      puts "Sending #{sig} signal to old unicorn master..."
-      Process.kill(sig, File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-    end
-  end
-
-  # Throttle the master from forking too quickly (for incremental kill-off only)
-  sleep 1
 end
 
 after_fork do |server, worker|
