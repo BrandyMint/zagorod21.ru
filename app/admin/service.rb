50b536547f87a226dc587fcda395923dd8167749
Danil Pismenny <danil@orionet.ru>
2013-12-07 12:15:24 +0400

    Fix services admin

--- app/admin/service.rb
+++ app/admin/service.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Service do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :title, :price
+  permit_params *Service.attribute_names
   #
   # or
   #
