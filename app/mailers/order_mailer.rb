db3fe2c957093d170c23cccadfe670906f9ca1c6
Alexey Naumov <alexsnaumov@gmail.com>
2013-12-24 15:58:28 +0400

    order email fix in production

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < BaseMailer
   def new_order_for_admin order, email
-    @order = order
     @house = order.house
+    @order = order.decorate
     @site = Settings.app.default_url_options.host
     mail(to: email, subject: "Новый заказ на сайте #{@site}")
   end
