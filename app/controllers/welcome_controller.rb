e2a6d853fb2233bda8d9f020537a4b4da2dfffb7
Alexey Naumov <alexsnaumov@gmail.com>
2013-12-25 14:53:42 +0400

    pagination, tr click

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,10 @@
 class WelcomeController < ApplicationController
-  PER_PAGE = 8
 
   def index
     hsq = HouseSearchQuery.new search_form, sort_form
     @all_houses = House.all.count
     @matched_houses = hsq.estimates.count
-    @estimates = Kaminari.paginate_array(hsq.estimates).page(params[:page]).per(PER_PAGE)
+    @estimates = Kaminari.paginate_array(hsq.estimates).page(params[:page]).per(HOUSES_PER_PAGE)
     render layout: 'application_wide'
   end
 
