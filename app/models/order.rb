4b9c95a27d9a91281a89d7b8e20c236f1691bcfc
Alexey Naumov <alexsnaumov@gmail.com>
2013-12-25 17:05:31 +0400

    form validation, sms in production

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,11 @@ class Order < ActiveRecord::Base
   belongs_to :house
 
   validates :name, :money_amount, presence: true
+  validates :date_from, :date_to, presence: true
+  validate do
+    self.errors.add :dates, 'Необходимо выбрать даты пребывания' unless date_from && date_from
+  end
+
   validates :email, email: true, allow_blank: true
   validates :phone, presence: true,  length: {is: 10}
 
