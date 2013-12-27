7d84eff1d93067aa264be6e9f681deb7f87c48ec
Danil Pismenny <danil@orionet.ru>
2013-12-27 11:07:46 +0400

    Fix Order factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -4,6 +4,8 @@ FactoryGirl.define do
   factory :order do
     house
     phone "1234567890"
+    date_from { Date.today }
+    date_to { Date.today + 1.day }
     name 'name'
     money_amount 10000
     people_quantity 2
