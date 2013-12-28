a671653aaece166f336bd491be033b5049590463
Danil Pismenny <danil@orionet.ru>
2013-12-28 15:39:44 +0400

    Минорные фиксы

--- app/decorators/house_decorator.rb
+++ app/decorators/house_decorator.rb
@@ -5,6 +5,12 @@ class HouseDecorator < Draper::Decorator
     resort.highlighted? ? 'warning' : ''
   end
 
+  def rooms_count
+    if rooms_count.present? && rooms_count >0
+      "#{rooms_count} комнат"
+    end
+  end
+
   def admin_link
     h.link_to 'edit', h.edit_admin_house_url(source), class: 'btn btn-small btn-warning' if h.current_user.can_create?(source)
   end
