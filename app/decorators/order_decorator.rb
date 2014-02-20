ebd0021f2ea40bedc94c9d6198121a30920acf84
heretge <vozdelaluz@gmail.com>
2014-02-20 18:14:45 +0400

    order decorator fix

--- app/decorators/order_decorator.rb
+++ app/decorators/order_decorator.rb
@@ -7,6 +7,10 @@ class OrderDecorator < Draper::Decorator
     "с #{from} по #{to}"
   end
 
+  def date_from_day
+    I18n.l source.date_from, format: :dots_separated
+  end
+
   def view_phone
     "+7 (#{phone[0,3]}) #{phone[3,3]}-#{phone[6,2]}-#{phone[8,2]} "
   end
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,7 +1,7 @@
 class OrderMailer < BaseMailer
   def new_order_for_admin order, email
     @house = order.house
-    @order = order.decorate
+    @order = OrderDecorator.new order
     @site = Settings.app.default_url_options.host
     mail(to: email, subject: "Новый заказ на сайте #{@site}")
   end
