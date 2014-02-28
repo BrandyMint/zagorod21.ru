a0812e139096995b60e697d44a796dc8e4f599bf
heretge <vozdelaluz@gmail.com>
2014-02-28 19:11:36 +0400

    order success design

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
 
+  include Concerns::HouseSearchConcern
+
   def show
+    search_for_houses show: 3, selected: true
     @order = OrderDecorator.new Order.find(params[:id])
   end
 
--- app/decorators/order_decorator.rb
+++ app/decorators/order_decorator.rb
@@ -11,6 +11,10 @@ class OrderDecorator < Draper::Decorator
     I18n.l source.date_from, format: :dots_separated
   end
 
+  def money_amount
+    h.money source.money_amount
+  end
+
   def view_phone
     "+7 (#{phone[0,3]}) #{phone[3,3]}-#{phone[6,2]}-#{phone[8,2]} "
   end
